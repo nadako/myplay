b159293221a7244c26876f6661a9eb8033c07a8c
Dan Korostelev <nadako@gmail.com>
2010-02-21 14:32:13 +0300

    Add package meta-data.

--- setup.py
+++ setup.py
@@ -1,10 +1,34 @@
 from setuptools import setup
 
+version = '0.1.0'
+
 setup(
-    name='MyPlay',
+    name='myplay',
+    version=version,
+    description='Simple music player service',
+    author='Dan Korostelev',
+    author_email='nadako@gmail.com',
+    url='http://code.google.com/p/myplay/', 
+    license='GPL v3',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: No Input/Output',
+        'Environment :: Console',
+        'Environment :: X11 Applications :: GTK',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.6',
+        'Topic :: Desktop Environment :: Gnome',
+        'Topic :: Multimedia :: Sound/Audio :: Players',
+    ],
+
     packages=['myplay'],
-    entry_points={'console_scripts': [
-                    'myplay = myplay.gui:main',
-                    'myplay-service = myplay.service:main'
-                 ]}
-    )
+    entry_points={
+        'console_scripts': [
+            'myplay = myplay.gui:main',
+            'myplay-service = myplay.service:main'
+        ]
+    }
+)
